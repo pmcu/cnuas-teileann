51df73d67972a3bc47a9e8bee0d48283b055c92c
pmcu <pmcumm@gmail.com>
2020-01-11 17:24:10 +0000

    fix eile

--- js/para.js
+++ js/para.js
@@ -1,4 +1,4 @@
 function myFunction (rd){
 var myElements = document.getElementsByTagName("div");
-document.getElementById("demo").innerHTML = "The text in the first paragraph is: " + myElements[rd].innerHTML;
+document.getElementById("demo").innerHTML =  myElements[rd].innerHTML;
 }
